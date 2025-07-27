0319d61cdb298a3afe59692176a088a86ec235fd
Samuel Gallego <samuelgallego441@gmail.com>
2025-07-26 19:06:41 -0500

    feat(front):creation of the route and profile selection page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage'
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/profiles" element={<ProfilePage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
