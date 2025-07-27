7151dc68b485741d3ef378cf8f95bc921ca94e3f
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2025-07-27 13:01:15 -0500

    feat(back): add logger

--- back/app/database/connection.py
+++ back/app/database/connection.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 import sqlite3
+from app.core.logger import logger
 
 DATABASE_PATH = Path("./db/movies.db")
 
 
 def get_db_connection(database_path: Path = DATABASE_PATH) -> sqlite3.Connection:
     try:
+        logger.info(f"Connecting to the database on the path: {database_path}")
         database_path.parent.mkdir(parents=True, exist_ok=True)
         conn = sqlite3.connect(str(database_path))
+        conn.row_factory = sqlite3.Row
+        logger.info(f"successful connection to the database {database_path}")
 
         return conn
     except sqlite3.Error as e:
--- back/main.py
+++ back/main.py
@@ -1,7 +1,8 @@
 from app.api.movie import router as movie_router
 from app.api.user import router as user_router
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from app.core.logger import logger
 
 app = FastAPI()
 
@@ -16,3 +17,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.middleware("http")
+async def log_request(request: Request, call_next):
+    logger.info(f"Incomming request: {request.method} {request.url}")
+
+    try:
+        response = await call_next(request)
+    except Exception as e:
+        logger.error(f"Request failed: {str(e)}")
+        raise
+
+    logger.info(f"Request completed: {response.status_code}")
+    return response
