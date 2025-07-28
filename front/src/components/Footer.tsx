53e64b92656082261e6a254677f81b1f5877613e
Emanuel Orozco <145817710+EmanuelOrozco@users.noreply.github.com>
2025-07-28 01:26:39 -0500

    Signup development (#24)
    * fix(front): add dependencies in correct folder
    
    * fix(front): delete test ProductCard.tsx
    
    * feat(front): initial struct signup
    
    * feat(front): add NavbarSignupFirst
    
    * feat(front): add NavbarSignupSecond
    
    * chore(front): add heroicons/react package
    
    * feat(front): add Registration page
    
    * feat(front): update Footer
    
    * feat(front): update HomePage and Registration

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full px-4 md:px-16 py-8 text-gray-400">
-      <div className="max-w-6xl mx-auto">
+    <footer className="w-full bottom-0 left-0 px-4 bg-gradient-to-b from-black/10 via-black/30 to-black/90 md:px-16 py-3 z-50 text-gray-400">
+      <div className="max-w-5xl mx-auto">
         <p className="text-center mb-4">
           ¿Preguntas? Llama al 01 800 917 1564
         </p>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-6">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mb-7">
           {[
             'Preguntas frecuentes',
             'Centro de ayuda',
---
+++ front/src/components/NavbarSignupSecond.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.webp';
+import { useNavigate } from 'react-router-dom';
+
+const NavbarSignup: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="fixed top-0 w-full left-0 bg-gradient-to-b from-black/90 via-black/80 to-black/50 z-50 px-2 py-3">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        <div className="flex items-center">
+          <Link to="/" className="mr-6">
+            <img 
+              src={logo} 
+              alt="FilmPeak Logo" 
+              className="h-20 md:h-24 object-contain"
+            />
+          </Link>
+        </div>
+        
+        <div className="flex items-center space-x-4">
+          <button className="px-4 py-2 rounded-lg bg-blue-600 text-white text-sm font-semibold transition hover:bg-blue-700 hover:shadow-lg hover:shadow-black-500/50 cursor-pointer"
+          onClick={() => navigate('/login')} >
+          Cerrar sesión
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarSignup;
