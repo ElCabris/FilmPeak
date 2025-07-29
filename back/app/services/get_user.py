91ce36f793867d229e5da0b8e665385c2d5ee9fb
Emanuel Cabrera Novoa <133041097+ElCabris@users.noreply.github.com>
2025-07-29 16:52:04 -0500

    feat(back): add image in profiles (#36)

--- back/app/services/get_user.py
+++ back/app/services/get_user.py
@@ -1,9 +1,16 @@
-from app.schemas.user import UserProfiles
+from app.schemas.user import UserProfiles, Profile
+
+
+def create_profile(profile_name: str, image: str) -> Profile:
+    return Profile(profile_name=profile_name, image=image)
 
 
 def create_user_profile_response(
     email: str, profiles: list[dict[str, str]]
 ) -> UserProfiles:
     return UserProfiles(
-        email=email, profiles=[profile["profile"] for profile in profiles]
+        email=email,
+        profiles=[
+            create_profile(profile["profile"], profile["image"]) for profile in profiles
+        ],
     )
